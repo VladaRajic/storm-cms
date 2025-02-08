63363aa9a52ceb79c3aa3eb8544814f3ea62647d
Vlada <vladacicevac@gmail.com>
2025-02-08 19:39:17 +0100

    init

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -43,24 +43,37 @@ class ProductService implements IProductService
 
     protected function storeImages(Product $product, array $images, string $mainImage): void
     {
-
         $product->images()->update(['is_main' => 0]);
-        foreach ($images as $key => $image) {
-            if(!is_array($image)){
-              //  dd($image);
-                $originalName = $image->getFileName();
-                $fileName = $image->storeAs(path: 'photos', name: \Str::random(40).'.'.$image->getClientOriginalExtension());
-                $isMain = $mainImage == $originalName;
-                $product->images()->save(new Image([
-                    'path' => $fileName,
-                    'is_main' => $isMain,
-                ]));
-            }else{
-                $imagePath = $image['path'];
 
-                $isMain = $mainImage == $imagePath;
-                Image::where('path', $imagePath)->update(['is_main' => $isMain]);
+        foreach ($images as $image) {
+            if ($this->isUploadedFile($image)) {
+                $this->handleUploadedFile($product, $image, $mainImage);
+            } else {
+                $this->updateExistingImage($image['path'], $mainImage);
             }
         }
+    }
+
+    protected function isUploadedFile($image): bool
+    {
+        return !is_array($image);
+    }
+
+    protected function handleUploadedFile(Product $product, $image, string $mainImage): void
+    {
+        $originalName = $image->getFileName();
+        $fileName = $image->storeAs('photos', \Str::random(40) . '.' . $image->getClientOriginalExtension());
+        $isMain = $mainImage === $originalName;
+
+        $product->images()->save(new Image([
+            'path' => $fileName,
+            'is_main' => $isMain,
+        ]));
+    }
+
+    protected function updateExistingImage(string $imagePath, string $mainImage): void
+    {
+        $isMain = $mainImage === $imagePath;
+        Image::where('path', $imagePath)->update(['is_main' => $isMain]);
     }
 }
